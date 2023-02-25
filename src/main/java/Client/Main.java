4e444c75338afb8213d92e1cea5e886a14433ee3
Andrey <pomeshikov-andrey@mail.ru>
2023-02-25 18:12:14 +0400

    Init commit

--- src/main/java/Client/Main.java
+++ src/main/java/Client/Main.java
@@ -1,23 +1,47 @@
 package Client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
         String host = "127.0.0.1";
         int port = 8195;
+        String message;
+        boolean isNameEntered = false;
+        boolean isAgeEntered = false;
+        boolean isSurnameEntered = false;
 
-        try (Socket clientSocket = new Socket(host, port);
-             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            out.println("Andrey");
-            String resp = in.readLine();
-            System.out.println(resp);
+        while (true) {
+            try (Socket clientSocket = new Socket(host, port);
+                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+            ) {
+                if (!isNameEntered) {
+                    System.out.println("Write your name");
+                    message = scr.nextLine();
+                    isNameEntered = true;
+                } else if (!isSurnameEntered) {
+                    System.out.println("Write your surname");
+                    message = scr.nextLine();
+                    isSurnameEntered = true;
+                } else if (!isAgeEntered) {
+                    System.out.println("Are you child? (yes/no)");
+                    message = scr.nextLine();
+                    isAgeEntered = true;
+                } else {
+                    System.out.println("Thank you for you answers");
+                    break;
+                }
+                out.println(message);
+            } catch (Exception e) {
+                System.out.println("Client side error " + e.getMessage());
+            }
         }
     }
 }
+
+
+
 
--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -3,7 +3,6 @@ package Server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -11,17 +10,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Server started");
         int port = 8195;
+        String nameReceived = "";
+        String ageReceived  = "";
+        String surnameReceived  = "";
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 try (Socket clientSocket = serverSocket.accept()) {
-                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                     {
-                        System.out.println("New connection accepted");
-                        final String name = in.readLine();
-                        out.println(String.format("Hi %s, your port is %d", name, clientSocket.getPort()));
+                        {
+                            if (nameReceived.isEmpty() && surnameReceived.isEmpty() && ageReceived.isEmpty()) {
+                                nameReceived  = in.readLine();
+                            } else if (!nameReceived.isEmpty() && surnameReceived .isEmpty() && ageReceived.isEmpty()) {
+                                surnameReceived = in.readLine();
+                            } else if (!nameReceived.isEmpty() && !surnameReceived.isEmpty() && ageReceived.isEmpty()) {
+                                ageReceived = in.readLine();
+                            } else {
+                                final String welcomeMessage = ageReceived.equals("no")
+                                        ? "Welcome to the adult zone, %s %s! Have a good rest, or a good working day!"
+                                        : "Welcome to the kids area,%s %s! Let's play!";
+                                System.out.printf(welcomeMessage, nameReceived, surnameReceived);
+                                break;
+                            }
+                        }
                     }
+                } catch (Exception e) {
+                    System.out.println("Server side error " + e.getMessage());
                 }
             }
         }
